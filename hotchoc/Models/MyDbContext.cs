d14792829839c214eb05b2f382092c54ab085dc8
Anton Ilyin <ailyin@dealerfire.com>
2018-11-22 22:39:08 +0200

    init

---
+++ hotchoc/Models/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+ 
+namespace hotchoc.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Phone> Phones { get; set; }
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ hotchoc/Models/Phone.cs
@@ -0,0 +1,7 @@
+﻿public class Phone
+{
+    public int Id { get; set; }
+    public string Name { get; set; } // название смартфона
+    public string Company { get; set; } // компания
+    public int Price { get; set; } // цена
+}
